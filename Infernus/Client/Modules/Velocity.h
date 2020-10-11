8c964821c76eda5cd98d5ac8461ea7bcf332ea5d
EchoHackCmd <EchoHackCmd@mail.com>
2020-10-11 16:18:53 +0100

    Update Velocity.h

--- Infernus/Client/Modules/Velocity.h
+++ Infernus/Client/Modules/Velocity.h
@@ -3,10 +3,18 @@
 
 class Velocity : public VModule {
 public:
-	Velocity() : VModule::VModule("Velocity", "Reduces knockback") {};
+	Velocity() : VModule::VModule("Velocity", "Reduces knockback") {
+		VWindowText* expandableText = new VWindowText("Velocity Modifiers");
+		expandableText->addExpandedItem(new VWindowSlider(&modifyX, 0.0f, 5.0f, "X: "));
+		expandableText->addExpandedItem(new VWindowSlider(&modifyY, 0.0f, 5.0f, "Y: "));
+		expandableText->addExpandedItem(new VWindowSlider(&modifyZ, 0.0f, 5.0f, "Z: "));
+		this->addWindowObj(expandableText);
+	};
 	void onLerpMotion(Vec3*);
+private:
+	float modifyX, modifyY, modifyZ;
 };
 
 void Velocity::onLerpMotion(Vec3* Vel) {
-	Vel->x = 0.0f, Vel->y = 0.0f, Vel->z = 0.0f;
+	Vel->x *= modifyX, Vel->y *= modifyY = modifyZ, Vel->z = 0.0f;
 };
