2b7fdcade3f85de6b82093c414dadd9f3b74f7f2
EchoHackCmd <EchoHackCmd@mail.com>
2020-10-19 12:32:13 +0100

    Update Criticals.h

--- Infernus/Client/Modules/Criticals.h
+++ Infernus/Client/Modules/Criticals.h
@@ -4,12 +4,27 @@
 class Criticals : public VModule {
 public:
 	Criticals() : VModule::VModule("Criticals", "Each hit is a critical hit") {};
-	void onPacket(void*, PacketType, bool*);
+	void onEnable();
+	void onDisable();
+private:
+	bool modified = false;
+	uintptr_t critsAddr = NULL;
 };
 
-void Criticals::onPacket(void* Packet, PacketType Type, bool* cancel) {
-	if (Type == PacketType::MovePlayerPacket) {
-		MovePlayerPacket* current = (MovePlayerPacket*)Packet;
-		current->onGround = false;
+void Criticals::onEnable() {
+	critsAddr = Utils::FindSignature("0F B6 86 ?? ?? 00 00 88 47 ?? 48 8B 96");
+	if (critsAddr != NULL) {
+		Utils::Patch((BYTE*)critsAddr, (BYTE*)"\xB8\x00\x00\x00\x00\x90\x90", 7);
+		modified = true;
 	};
 };
+
+void Criticals::onDisable() {
+	if (modified) {
+		Utils::Patch((BYTE*)critsAddr, (BYTE*)"\x0F\xB6\x86\xA0\x01\x00\x00", 7);
+		modified = false;
+	};
+};
+
+//0F B6 86 ?? ?? 00 00 88 47 ?? 48 8B 96 ?? ?? ?? ??
+//
