29780955677d5ae41f1bab2cde20492cc0e158d7
OvermindMC <Overmind@mail.com>
2020-11-02 15:03:33 +0000

    Update ClientHandler.cpp

--- Infernus/Client/ClientHandler.cpp
+++ Infernus/Client/ClientHandler.cpp
@@ -271,10 +271,6 @@ void ClientHandler::InitModuleFiles() {
 
 		Module->wasEnabled = Module->isEnabled = enabledModule;
 		Module->key = GetModuleKeyFromFile(Module);
-
-		if (Module->key != NULL) {
-			Utils::DebugFileLog(Module->name + " : " + Utils::convert_ASCII(Module->key));
-		};
 	};
 };
 
