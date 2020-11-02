cb8a270a499a3636a95e44c257cbb1c8a1c989f0
OvermindMC <Overmind@mail.com>
2020-11-02 18:33:48 +0000

    Update Nametags.h

--- Infernus/Client/Modules/Nametags.h
+++ Infernus/Client/Modules/Nametags.h
@@ -5,25 +5,34 @@ class Nametags : public VModule {
 public:
 	Nametags() : VModule::VModule("Nametags", "Display entity nametags even when they're crouching") {};
 	void onRender();
+	bool canContinueRendering();
+};
+
+bool Nametags::canContinueRendering() {
+	if (Minecraft::GetClientInstance() == nullptr)
+		return false;
+	if (Minecraft::GetLocalPlayer() == nullptr)
+		return false;
+	if (!Minecraft::GetClientInstance()->MinecraftGame->canUseKeys)
+		return false;
+	return true;
 };
 
 void Nametags::onRender() {
-	auto C_instance = Minecraft::GetClientInstance();
-	auto L_Player = Minecraft::GetLocalPlayer();
-	bool canUseKeys = C_instance != nullptr ? C_instance->MinecraftGame->canUseKeys : false;
-
-	if (L_Player != nullptr && canUseKeys && RenderUtils::canUseRenderUtils()) {
-		auto Players = Minecraft::FetchPlayers();
-
-		if (!Players->empty()) {
-			for (auto Player : *Players) {
-				if (L_Player != nullptr) {
-					if (C_instance->isValidTarget(Player)) {
-						float distance = Utils::distanceVec3(*Player->getPos(), *C_instance->LocalPlayer()->getPos());
-						RenderUtils::DrawNametag(Player, MC_Colour(255, 255, 255), MC_Colour(50, 50, 50), MC_Colour(30, 160, 200), fmax(0.6f, 3.f / distance), C_instance);
-					};
+	std::vector<Actor*>* Entities = Minecraft::FetchPlayers();
+	if (canContinueRendering()) {
+		for (auto Entity : *Entities) {
+			if (canContinueRendering()) {
+				if (Minecraft::GetClientInstance()->isValidTarget(Entity)) {
+					float distance = Utils::distanceVec3(*Entity->getPos(), *Minecraft::GetLocalPlayer()->getPos());
+					RenderUtils::DrawNametag(Entity, fmax(0.6f, 3.f / distance), Minecraft::GetClientInstance());
 				};
+			}
+			else {
+				Entities->clear();
+				break;
 			};
 		};
+		RenderUtils::FlushText();
 	};
 };
