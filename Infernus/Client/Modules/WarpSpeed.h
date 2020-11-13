b281fb24593812ceed35ae0bbaa81376394d5835
EchoHackCmd <EchoHackCmd@mail.com>
2020-11-13 21:42:55 +0000

    Better collisions for WarpSpeed

--- Infernus/Client/Modules/WarpSpeed.h
+++ Infernus/Client/Modules/WarpSpeed.h
@@ -40,16 +40,35 @@ void WarpSpeed::onLoop() {
 
 bool WarpSpeed::isColliding(Vec3 blockPos) {
 	LocalPlayer* Player = Minecraft::GetLocalPlayer();
+	std::vector<int> inFrontBlocks = { 0 /*Air*/, 30 /*Cobweb*/, 31 /*Tall Grass */, 50 /*Torch*/, 63 /*Sign*/, 90 /*Portal*/, 106 /*Vine*/, 175 /*Double Plant*/, 486 /*Weeping Vines*/, 542 /*Twisted Vines*/, 542 /*Twisted Vines*/ };
+	std::vector<int> belowBlocks = { 0 /*Air*/, 6 /*Sappling*/, 27 /*Golden Rail*/, 28 /*Detector Rail*/, 30 /*Cobweb*/, 31 /*Tall Grass */, 32 /*Dead Bush*/, 37 /*Yellow Flower*/, 38 /*Red Flower*/, 39 /*Brown Mushroom*/, 40 /*Red Mushroom*/, 50 /*Torch*/, 51 /*Fire*/, 55 /*Redstone Wire*/, 59 /*Wheat*/, 63 /*Sign*/, 65 /*Ladder*/, 66 /*Rail*/, 68 /*Wall Sign*/, 69 /*Lever*/, 70 /*Stone Pressure Plate*/, 72 /*Wooden Pressure Plate*/, 75 /*Unlit Redstone Torch*/, 76 /*Lit Redstone Torch*/, 77 /*Stone Button*/, 78 /*Snow Layer*/, 83 /*Reeds*/, 90 /*Portal*/, 104 /*Pumpkin Stem*/, 105 /*Melon Stem*/, 106 /*Vine*/, 119 /*End Portal*/, 126 /*Activator Rail*/, 143 /*Wooden Button*/, 147 /*Golden Pressure Plate*/, 148 /*Iron Pressure Plate*/, 171 /*Carpet*/, 175 /*Double Plant*/, 385 /*Sea Grass*/, 386 /*Coral*/, 388 /*Coral Fan*/, 389 /*Coral Fan Dead*/, 393 /*Kelp*/, 395 /*Acacia Button*/, 396 /*Birch Button*/, 397 /*Dark Oak Button*/, 398 /*Jungle Button*/, 399 /*Spruce Button*/, 462 /*Sweet Berry Bush*/, 478 /*Crimson Roots*/, 479 /*Warped Roots*/, 483 /*Crimson Fungus*/, 484 /*Warped Fungus*/, 486 /*Weeping Vines*/, 492 /*Soul Fire*/, 493 /*Nether Sprouts*/, 505 /*Crimson Sign*/, 506 /*Warped Sign*/, 507 /*Crimson Wall Sign*/, 508 /*Warped Wall Sign*/, 515 /*Crimson Button*/, 516 /*Warped Button*/, 517 /*Crimson Pressure Plate*/, 518 /*Warped Pressure Plate*/, 523 /*Soul Torch*/, 542 /*Twisted Vines*/, 550 /*Polished Pressure Plate*/ };
 	if (Player != nullptr) {
 		Vec3 currPos = *Player->getPos();
 		Block* block = Player->BlockSource->getBlock(Vec3(blockPos.x, currPos.y - 1.0f, blockPos.z));
-		if (block->toLegacy()->blockID > 0) {
-			return true;
-		}
-		else {
-			block = Player->BlockSource->getBlock(blockPos);
-			return block->toLegacy()->blockID > 0;
+		int blockID = block->toLegacy()->blockID;
+
+		bool colliding = true;
+
+		for (auto ID : belowBlocks) { //Check just below head
+			if (blockID == ID) {
+				colliding = false;
+				break;
+			};
 		};
+
+		if (!colliding) {
+			Block* block = Player->BlockSource->getBlock(blockPos);
+			int blockID = block->toLegacy()->blockID;
+
+			for (auto ID : inFrontBlocks) { //Check at head position
+				if (blockID == ID) {
+					colliding = false;
+					break;
+				};
+			};
+		};
+
+		return colliding;
 	};
 	return false;
 };
@@ -65,6 +84,9 @@ void WarpSpeed::sendPacket(Vec3 blockPos) {
 };
 
 void WarpSpeed::runTask(LocalPlayer* Player, Vec3 blockPos) {
-	if (sendPackets) sendPacket(blockPos);
 	Player->setPos(&blockPos);
+
+	if (sendPackets) {
+		sendPacket(blockPos);
+	};
 };
